925e78c094426bb06ce87fc07f2a9ebe12b90049
farukvai850@gmail.com <farukvai850@gmail.com>
2021-02-08 16:40:46 +0600

    template js

---
+++ defultvalue.js
@@ -0,0 +1,5 @@
+function add(a,b=0){
+    return a+b
+}
+const result=add(2,5);
+console.log(result);
---
+++ template.js
@@ -0,0 +1,15 @@
+const firstName = "Omor";
+const lastname  = "Faruk";
+
+const fullName = `${firstName} ${lastname}`
+
+console.log(fullName);
+
+const text = `I Love You Sima
+You are my elder sister
+        But still i am missing you
+    Why its happen with me? 
+            Will you please explain it ${1+4+3}?
+            `;
+
+console.log(text);
